eb07a4fba70a5abd58f88d5e45d5b54e028459bf
skorobahatko <maxim.skorobagatko@gmail.com>
2020-08-21 00:25:52 +0300

    added comments and path to fill files

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const pathh = require('path');
 
+// директорія в яку будуть збиратися всі файли
+let pathTo = 'fillMe';
+
+// початкова директорія для пошуку
 let initialPath = pathh.join(process.cwd(), 'nativeDir');
+
 recursion = function (path) {
     fs.readdir(path, (err, files) => {
         for (let i in files) {
             let pathToFile = path + '/' + files[i];
             fs.lstat(pathToFile, (err1, simpleFile) => {
                 if (err1) return console.log (err1);
-             if (simpleFile.isDirectory()) {
-                 recursion(pathToFile);
-             } else {
-                 console.log (pathToFile);
-                 fs.rename(pathToFile, pathh.join(process.cwd(), 'fillMe', files[i]), (err) => {
+                if (simpleFile.isDirectory()) {
+                    recursion(pathToFile);
+                } else {
+                    // процес переходу в іншу директорію файлів
+                 fs.rename(pathToFile, pathh.join(process.cwd(), pathTo, files[i]), (err) => {
                      if (err) console.log (err);
                  })
              }
